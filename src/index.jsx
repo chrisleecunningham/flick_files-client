876ce5fd31599b6a1a61cce3b165a648e5aa400b
chrisleecunningham <103608696+chrisleecunningham@users.noreply.github.com>
2023-03-02 10:00:53 -0800

    create
    created main html, jsx, scss files

---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+import { createRoot } from 'react-dom/client';
+
+// Import statement to indicate that you need to bundle './index.scss'
+import "./index.scss";
+
+// Main component (will eventually use all others)
+const flickFilesApplication = () => {
+  return (
+    <div className="flick-files">
+      <div>Good morning</div>
+    </div>
+  );
+};
+
+// Finds the root of your app
+const container = document.querySelector("#root");
+const root = createRoot(container);
+
+// Tells React to render your app in the Root DOM element
+root.render(<flickFilesAppplication/>);
