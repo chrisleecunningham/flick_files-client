b74d2a7e9dd69e6d7dbd07a93cd9c65ae9679fd0
chrisleecunningham <103608696+chrisleecunningham@users.noreply.github.com>
2023-03-06 12:33:55 -0800

    json

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,19 @@
 import { createRoot } from "react-dom/client";
-
 import { MainView } from "./components/main-view/main-view";
+import Container from "react-bootstrap/Container";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.scss";
 
 // Import statement to indicate that you need to bundle './index.scss'
 import "./index.scss";
 
 // Main component (will eventually use all others)
 const FlickFilesApplication = () => {
-  return <MainView />
+  return (
+    <Container>
+      <MainView />
+    </Container>
+  )
 };
 
 // Finds the root of your app
