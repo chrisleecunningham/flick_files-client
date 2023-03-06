ba95b0abdd0d714c8ec3b29fb8a61bbe1fb78a6a
chrisleecunningham <103608696+chrisleecunningham@users.noreply.github.com>
2023-03-06 12:44:23 -0800

    Update main-view.jsx
    edited logout button

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -5,6 +5,7 @@ import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 export const MainView = () => {
   const storedUser = JSON.parse(localStorage.getItem("user"));
@@ -79,7 +80,7 @@ export const MainView = () => {
         </>
       )}
         
-      <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
+      <Button variant="secondary" onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</Button>
 
     </Row>
   );
